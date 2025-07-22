6c0f2dcc6dcdcc290b4d2a53754eef8ab9ed5d18
Leonel Berdichevsky <221699312+LeonelBerdi@users.noreply.github.com>
2025-07-22 13:58:03 +0200

    Seccion2.11 - Variables Declaracion

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log('Hola Mundo');
+
+// console.log( console.log('Hola Mundo') );
+
+let a = 10;     // varibles se puede declarar de una en una o
+
+let b = 20,     // multiples separadas por "," 
+    c = 10, 
+    d = 10,    
+    x = a + b;
+
+
+console.log( x );
+
+
+
+/* EJERCICO DE CLASE
+var b = 10;     //estas son todas las formas de crear variables
+const c = 10;   //estas son todas las formas de crear variables 
+                esta ultima es una constante 
+
+c = 20;       // no se puede modificar el valor de una constante
+
+a = 20;
+b = 30;
+*/
