55937a366e0f40173808b7a496ff2f08708e50d0
Leonel Berdichevsky <221699312+LeonelBerdi@users.noreply.github.com>
2025-07-28 16:21:35 +0200

    Seccion4.35 Alternativas a if-else

--- assets/js/if-else.js
+++ assets/js/if-else.js
@@ -48,7 +48,45 @@ if ( dia === 0 ) {
     console.log('dia de la semana valor invalido');
 };
 
+// Ejercicio sin usar If Else, o Switch, unicamente objetos mostrar el dia de la semana
 
+let dia2 = 3; // 0:domingo, 1:lunes .....
+
+let diaLetras = ['Domingo' ,'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado'];
+
+console.log('Hoy es el dia:', diaLetras[dia2]);
 
 console.log('Fin de programa');
 
+// solucion con objetos
+
+const diaLetras2 = {
+   0 : 'Domingo',
+   1 : 'Lunes',
+   2 : 'Martes',
+   3 : 'Miercoles',
+   4 : 'Jueves',
+   5 : 'Viernes',
+   6 : 'Sabado',
+};
+
+console.log('Hoy es el dia:', diaLetras2[dia2]);
+
+//QUE PASA SI EL VALOR DE dia no es 0-6 me devuelve "undefined"
+//SOLUCION
+let dia3 = 8; // 0-6 (Domingo...Sabado), 8 no es un dia valido
+console.log('Hoy es el dia:', diaLetras2[dia3] || 'NO DEFINIDO'); // el operador "or" ("||"), va a preguntar si la operacion anterior tiene un valor si es asi no se va a ejecutar nunca pero si el resultado es "null" o "undefined" se va a ejecutar.
+
+// solucion con objetos y funciones 
+const diaLetras3 = {
+   0 : ()=> 'Domingo - 0',
+   1 : ()=> 'Lunes - 1',
+   2 : ()=> 'Martes - 2',
+   3 : ()=> 'Miercoles - 3',
+   4 : ()=> 'Jueves - 4',
+   5 : ()=> 'Viernes - 5',
+   6 : ()=> 'Sabado - 6',
+};
+
+console.log('Hoy es el dia:', diaLetras3[dia2]); // aqui apunto a la funcion pero no al resueltado 
+console.log('Hoy es el dia:', diaLetras3[dia2]()); // aqui ejecuto la funcion le mando un parametro vacio
