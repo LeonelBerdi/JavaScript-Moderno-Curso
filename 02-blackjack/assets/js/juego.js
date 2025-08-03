0888e971496022f22f23852d53b9d77f2cf22e27
Leonel Berdichevsky <221699312+LeonelBerdi@users.noreply.github.com>
2025-08-03 18:40:58 +0200

    Seccion5.51 Pedir Cartas

--- 02-blackjack/assets/js/juego.js
+++ 02-blackjack/assets/js/juego.js
@@ -9,6 +9,7 @@ let deck         = [];
 const tipos      = ['C','D','H','S']; // Club - Diamond - Hearts - Spades 
 const especiales = ['A','J','Q','K']; // AS - JOKER - QUEEN - KING 
 
+// Esta funcion crea una nueva baraja.
 const crearDeck= () => {
 
     for ( let i = 2; i <=10 ; i++ ) {
@@ -29,11 +30,40 @@ const crearDeck= () => {
         }
     };
     */
-
-    console.log( deck );
+//  console.log( deck );            Imprimir en consola el mazo ordenado.
     deck = _.shuffle( deck );
     console.log( deck );
     return deck;
 };
 
 crearDeck();
+
+
+// Esta funcion me permite tomar una carta
+const pedirCarta = () => {
+
+    if ( deck.length === 0 ) {
+        throw 'No hay cartas en el deck';
+    };
+
+    const carta = deck.pop();
+    
+    console.log( deck );
+    console.log({ carta });
+    return carta;
+};
+
+
+/*
+deck = [];                                      Para comrpobar que el trow funciona inicializo el mazo sin cartas
+pedirCarta();
+
+const totalCartas = deck.length;                Otra opcion para comprobar el trow es vaciar el mazo con un for de cartas
+for ( let i = 0; i < totalCartas +1 ; i ++) {
+    pedirCarta();
+};
+*/
+
+
+
+
