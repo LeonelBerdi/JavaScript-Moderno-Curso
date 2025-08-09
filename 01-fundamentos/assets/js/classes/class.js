8d8927b0319c818a75bdc7f8180d89337a5e995b
Leonel Berdichevsky <221699312+LeonelBerdi@users.noreply.github.com>
2025-08-09 17:55:49 +0200

    Seccion7.74 Metodos en las Clases

--- 01-fundamentos/assets/js/classes/class.js
+++ 01-fundamentos/assets/js/classes/class.js
@@ -1,22 +1,39 @@
 
 
 class Persona {
-    
-    nombre = '';    //valor por defecto un string vacio
+    /* ESTAS PROPIEDADES SE RECIBEN VIA PARAMETROS EN EL CONSTRUCTOR NO ES NECESARIO DECLARARLAS
+    nombre = '';
     codigo = '';
     frase = '';
-    
+    */
+    comida ='';     //Como la propiedad no recibe valor del constructor la de declarar
     constructor( nombre = 'Sin Nombre', codigo = 'Sin Código', frase = 'Sin Frase' ) {  //valor por defecto
-        
+      
         this.nombre = nombre;
         this.codigo = codigo;
-        this.frase = frase;
+        this.frase = frase;    
+
     };
+
+    quienSoy() {
+        console.log(`Soy ${ this.nombre } y mi identidad es ${ this.codigo } `);
+    }
+
+    miFrase() {
+        this.quienSoy();
+        console.log(`${ this.codigo } dice: ${ this.frase }`);
+    }
+
 };
 
 const spiderman = new Persona( 'Peter Parker', 'Spiderman', 'Tu amigo y vecino');
 console.log(spiderman);
 const ironman = new Persona( 'Tony Stark', 'Ironman', 'Yo soy Ironman');
 console.log(ironman);
-
+/*
+spiderman.quienSoy();
+ironman.quienSoy();
+*/
+spiderman.miFrase();
+ironman.miFrase();
 
