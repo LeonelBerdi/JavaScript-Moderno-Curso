4dad98d60ff1f359db7ffab669e2f053f387fcb7
Leonel Berdichevsky <221699312+LeonelBerdi@users.noreply.github.com>
2025-08-10 13:32:12 +0200

    Seccion7.76 Propiedades, gets y metodos Estaticos (de las clases)

--- 01-fundamentos/assets/js/classes/class.js
+++ 01-fundamentos/assets/js/classes/class.js
@@ -1,17 +1,28 @@
 
 
 class Persona {
-   
+    static _conteo = 0; //Propiedad de tipo estatica
+    static get getConteo() {
+        return Persona._conteo + ' instancias'; //Hago referencia a la propiedad de la clase 
+    };
+
+    static mensaje() {                      //Es un metodo de la clase lo llamo desde Persona y lleva ().
+        console.log( this.nombre );         //Al hacer referencia a una propiedad instanciada me devolvera undefined
+        console.log('Hola a todos, soy un método estático');    
+    };
+
     nombre = '';
     codigo = '';
     frase = '';
     comida ='';     
 
     constructor( nombre = 'Sin Nombre', codigo = 'Sin Código', frase = 'Sin Frase' ) {  
+        
         this.nombre = nombre;
         this.codigo = codigo;
         this.frase = frase;    
 
+        Persona._conteo++;      //Incremento en uno el contador cada vez que se ejecuta el constructor asii se cuantas instacias de este tipo se han creado
     };
 
     set setComidaFavorita( comida ) {
@@ -34,6 +45,10 @@ class Persona {
 };
 
 const spiderman = new Persona( 'Peter Parker', 'Spiderman', 'Tu amigo y vecino');
+const ironman = new Persona( 'Tony Stark', 'Ironman', 'Yo soy Ironman');
+
+
+
 //console.log(spiderman);
 /*
 const ironman = new Persona( 'Tony Stark', 'Ironman', 'Yo soy Ironman');
@@ -47,7 +62,18 @@ spiderman.miFrase();
 spiderman.setComidaFavorita = 'el pie de cereza de la tia may';
 
 //spiderman.comida = 'Duende Verde';
-console.log(spiderman);
-console.log( spiderman.getComidaFavorita );
+// console.log(spiderman);
+// console.log( spiderman.getComidaFavorita );
 
+//Persona._conteo = 2;    //se han inicializado 2 instacis de tipo Persona
 
+// console.log('Conteo estatico', Persona._conteo );
+
+console.log( Persona.getConteo );
+
+Persona.mensaje();
+
+Persona.propiedadExterna = 'Hola Mundo';
+
+console.log( Persona );
+console.log( Persona.propiedadExterna );
