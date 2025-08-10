975d07ddebb70f4d56dd0b47cd90e4dad750a234
Leonel Berdichevsky <221699312+LeonelBerdi@users.noreply.github.com>
2025-08-10 11:39:52 +0200

    Seccion7.75 Sets y Gets

--- 01-fundamentos/assets/js/classes/class.js
+++ 01-fundamentos/assets/js/classes/class.js
@@ -1,39 +1,53 @@
 
 
 class Persona {
-    /* ESTAS PROPIEDADES SE RECIBEN VIA PARAMETROS EN EL CONSTRUCTOR NO ES NECESARIO DECLARARLAS
+   
     nombre = '';
     codigo = '';
     frase = '';
-    */
-    comida ='';     //Como la propiedad no recibe valor del constructor la de declarar
-    constructor( nombre = 'Sin Nombre', codigo = 'Sin Código', frase = 'Sin Frase' ) {  //valor por defecto
-      
+    comida ='';     
+
+    constructor( nombre = 'Sin Nombre', codigo = 'Sin Código', frase = 'Sin Frase' ) {  
         this.nombre = nombre;
         this.codigo = codigo;
         this.frase = frase;    
 
     };
 
+    set setComidaFavorita( comida ) {
+        this.comida = comida.toUpperCase(); 
+    };
+
+    get getComidaFavorita() {
+        return `La comida Favoria de ${ this.nombre} es ${ this.comida }`;
+    };
+
     quienSoy() {
         console.log(`Soy ${ this.nombre } y mi identidad es ${ this.codigo } `);
-    }
+    };
 
     miFrase() {
         this.quienSoy();
         console.log(`${ this.codigo } dice: ${ this.frase }`);
-    }
+    };
 
 };
 
 const spiderman = new Persona( 'Peter Parker', 'Spiderman', 'Tu amigo y vecino');
-console.log(spiderman);
+//console.log(spiderman);
+/*
 const ironman = new Persona( 'Tony Stark', 'Ironman', 'Yo soy Ironman');
 console.log(ironman);
-/*
+
 spiderman.quienSoy();
 ironman.quienSoy();
 */
 spiderman.miFrase();
-ironman.miFrase();
+//ironman.miFrase();
+spiderman.setComidaFavorita = 'el pie de cereza de la tia may';
+
+//spiderman.comida = 'Duende Verde';
+console.log(spiderman);
+console.log( spiderman.getComidaFavorita );
+
 
