a7eccce36ff69a30a216a1ebc9430cb61aa49063
Leonel Berdichevsky <221699312+LeonelBerdi@users.noreply.github.com>
2025-08-17 19:03:10 +0200

    Seccion10.114 Metodos del Store

--- 05-todo-app/src/store/todo.store.js
+++ 05-todo-app/src/store/todo.store.js
@@ -10,7 +10,9 @@ const state = {
     todos: [
         new Todo('Piedra del alma'),
         new Todo('Piedra del infinito'),
-        new Todo('Piedra del tiempo'),       
+        new Todo('Piedra del tiempo'),
+        new Todo('Piedra del poder'),
+        new Todo('Piedra del realidad'),
     ],
     filter: Filters.All,    
 }
@@ -22,7 +24,86 @@ const initStore = () => {
     console.log('InitStore 🥑');
 }
 
-export default {
-    initStore,
-
+const loadStore = () => {
+    throw new Error('Not implemented');
 }
+
+const getTodos = ( filter = Filters.All ) => {
+    
+    switch( filter ) {
+        case Filters.All:
+            return [...state.todos];
+
+        case Filters.Completed:
+            return state.todos.filter( todo => todo.done );
+
+        case Filters.Pending:
+            return state.todos.filter( todo => !todo.done );
+        
+        default:
+            throw new Error(`Option ${ filter } is not valid.`);
+    }
+}
+
+/**
+ * 
+ * @param {String} description 
+ */
+const addTodo = ( description ) => {
+    if ( !description ) throw new Error('Description is required');
+    
+    state.todos.push( new Todo(description) );
+}
+
+/**
+ * 
+ * @param {String} todoId Todo identifier
+ */
+const toggleTodo = ( todoId ) => {
+
+    state.todos = state.todos.map( todo => {
+        if( todo.id === todoId ) {
+            todo.done = !todo.done;
+        }
+        return todo;
+    });
+    
+}
+
+/**
+ * 
+ * @param {String} todoId Todo identifier
+ */
+const deleteTodo = ( todoId ) => {
+    state.todos = state.todos.filter( todo => todo.id !== todoId );
+}
+
+const deleteCompleted = () => {
+    state.todos = state.todos.filter( todo => todo.done );
+}
+
+/**
+ * 
+ * @param {Filters} newFilter 
+ */
+
+const setFilter = ( newFilter = Filters.all ) => {
+    state.filter = newFilter;
+}
+
+const getCurrentFilter = () => {
+    return state.filter;
+}
+
+
+export default {
+    addTodo,
+    deleteCompleted,
+    deleteTodo,
+    getCurrentFilter,
+    getTodos,
+    initStore,
+    loadStore,
+    setFilter,
+    toggleTodo,
+};
