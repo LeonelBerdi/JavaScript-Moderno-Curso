301ccac5b4eb378fc81d2cd31a7d03cc8fed53f5
Leonel Berdichevsky <221699312+LeonelBerdi@users.noreply.github.com>
2025-08-21 00:12:00 +0200

    Seccion10.122 Cambiar Filtros

--- 05-todo-app/src/todos/app.js
+++ 05-todo-app/src/todos/app.js
@@ -1,11 +1,12 @@
 import html from './app.html?raw';
-import todoStore from '../store/todo.store';
+import todoStore, { Filters } from '../store/todo.store';
 import { renderTodos } from './use-cases';
 
 const elementIDs = {
     ClearCompletedButton: '.clear-completed',
     TodoList: '.todo-list',
     NewTodoInput: '#new-todo-input',
+    TodoFilters: '.filtro',
 
 
 }
@@ -38,6 +39,7 @@ export const App = ( elementId ) => {
     const newDescriptionInput = document.querySelector( elementIDs.NewTodoInput );
     const todoListUL = document.querySelector( elementIDs.TodoList );
     const ClearCompletedButton = document.querySelector( elementIDs.ClearCompletedButton );
+    const filtersLIs = document.querySelectorAll( elementIDs.TodoFilters );
 
     //Listeners
     newDescriptionInput.addEventListener('keyup', ( event ) => {
@@ -80,6 +82,31 @@ export const App = ( elementId ) => {
     ClearCompletedButton.addEventListener('click', () => {
         todoStore.deleteCompleted();
         displayTodos();
+    });
+
+    filtersLIs.forEach( element => { 
+
+        element.addEventListener('click', (element) => {
+            filtersLIs.forEach( el => el.classList.remove('selected'));
+            element.target.classList.add('selected');
+
+            switch( element.target.text ){
+                case 'Todos':
+                    todoStore.setFilter( Filters.All )
+                break;
+                case 'Pendientes':
+                    todoStore.setFilter( Filters.Pending )
+                break;
+                case 'Completados':
+                    todoStore.setFilter( Filters.Completed )
+                break;
+            }
+
+            displayTodos();
+
+        });
+
+
     });
 
 
