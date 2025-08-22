d467c42f59ebc20b66fd9463e65a34608f021626
Leonel Berdichevsky <221699312+LeonelBerdi@users.noreply.github.com>
2025-08-22 19:11:02 +0200

    Seccion11.130 Configuraciones Previas

---
+++ 06-advanced/src/concepts/demo.js
@@ -0,0 +1,12 @@
+
+/**
+ * 
+ * @param {HTMLDivElement} element Aqui voy a recibir el elemento Html es el espacio en el que voy a querer renderizar lo que sea
+ */
+export const demoComponent = ( element ) => {
+
+    console.log('democomponent');
+
+    
+}
+
