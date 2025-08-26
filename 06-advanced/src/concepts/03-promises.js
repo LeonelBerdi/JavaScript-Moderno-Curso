59ddc390a15747324c60c5f8beabb38353ce8dce
Leonel Berdichevsky <221699312+LeonelBerdi@users.noreply.github.com>
2025-08-26 17:58:10 +0200

    Seccion11.136 Promise Hell

--- 06-advanced/src/concepts/03-promises.js
+++ 06-advanced/src/concepts/03-promises.js
@@ -9,6 +9,15 @@ export const promiseComponent = ( element ) => {
     const renderHero = ( hero ) => {
         element.innerHTML = hero.name;
     }
+    
+    //Creo una funcion para mostrar 2 heroes
+    const renderTwoHeros= ( hero1, hero2 ) => {
+        element.innerHTML = `
+        <h3>${hero1.name}</h3>
+        <h3>${hero2.name}</h3> 
+        `;
+    }
+
     //Esta funcion es para manejar el error
     const renderError = ( error ) => {
         element.innerHTML = `
@@ -18,10 +27,11 @@ export const promiseComponent = ( element ) => {
     } 
     const id1 ='5d86371f25a058e5b1c8a65e';
     const id2 ='5d86371f233c9f2425f16916';
-
+    /* Modifico el codigo para 2 heroes!!
     findHero( id1 )
         .then( renderHero )
         .catch( renderError );
+    */
         /* PROMESAS COMO CONSUMIRLAS OBTENER SU VALOR
         .then = me da el valor de la promesa si se ha resuleto "resolve"
         .catch = me da el valor de la promesa si hay un error "reject"
@@ -36,6 +46,32 @@ export const promiseComponent = ( element ) => {
         Optimizo:
         .catch( renderError );
         */
+    /*OPTIMIZACION DE CODIGO QUITO LAS VARIABLES Y LAS PASO DIRECTO AL RENDER 
+    let hero1, hero2; //defino ambos heroes como undefined
+
+    findHero( id1 )
+        .then( ( hero1n ) => {
+            hero1 = hero1n;   //el hero1 es igual al hero que recibo de mi promesa
+
+            findHero( id2 ) //busco el hero2
+                .then( hero2n => {
+                    hero2 = hero2n;
+                    renderTwoHeros(hero1,hero2);
+                } )
+                .catch( renderError );
+        } )
+        .catch( renderError );
+    */
+    findHero( id1 )
+        .then( ( hero1 ) => {
+            
+            findHero( id2 ) //busco el hero2
+                .then( hero2 => {
+                    renderTwoHeros(hero1,hero2);
+                } )
+                .catch( renderError );
+        } )
+        .catch( renderError );
 }
 
 /**
