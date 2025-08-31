1924fab6d919b93f0149e8b54af16b5cce154927
Leonel Berdichevsky <221699312+LeonelBerdi@users.noreply.github.com>
2025-08-31 17:46:08 +0200

    Seccion11.146 Ejemplo de funcion Generadora

--- 06-advanced/src/concepts/09-generators.js
+++ 06-advanced/src/concepts/09-generators.js
@@ -5,16 +5,33 @@
  */
 export const generatorFunctionsComponent = ( element ) => {
 
-   const myGenerator = myFirstGeneratorFunction();
+    //const myGenerator = myFirstGeneratorFunction();
+    //console.log( myGenerator.next());
+    const genId = idGenerator();
 
-    console.log( myGenerator.next());
-    console.log( myGenerator.next());
-    console.log( myGenerator.next());
-    console.log( myGenerator.next());
-    console.log( myGenerator.next());
-    console.log( myGenerator.next());
+    const button = document.createElement('button');
+    button.innerText = 'Click me';
+    element.append( button );
+
+    const renderButton = () => {
+        const { value } = genId.next();
+        button.innerText = `Click ${ value }`;
+    } 
+    /*Sepuede optimizar estoy en un caso donde mi funcion que voy a llamar no tiene ningun argumento perfectamente puedo no toear el arguemnto event y darle como parametro la funcion. 
+    button.addEventListener('click', (event) => renderButton() );
+    button.addEventListener('click', () => renderButton() ); como no tewngo argumentos envio como referencia la funcion
+    button.addEventListener('click', renderButton);
+    */
+    button.addEventListener('click', renderButton);
 }
 
+function* idGenerator() {   //Generar id infinitos
+    let currentId = 0;
+    while(true) {
+        yield ++currentId;  //Asi el primer valor es 1 si lo pongo como "currentId++" el primer valor sera 0.
+    }
+
+} 
 
 
 
