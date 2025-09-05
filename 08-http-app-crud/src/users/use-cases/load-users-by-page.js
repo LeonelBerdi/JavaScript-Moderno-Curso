719c8a2c347cce6450740ded35ea610c7052b07d
Leonel Berdichevsky <221699312+LeonelBerdi@users.noreply.github.com>
2025-09-05 19:28:28 +0200

    Seccion13.164 Mappers

--- 08-http-app-crud/src/users/use-cases/load-users-by-page.js
+++ 08-http-app-crud/src/users/use-cases/load-users-by-page.js
@@ -1,16 +1,22 @@
-
+import { localhostUserToModel } from "../mappers/localhost-user.mapper.js";
+import { User } from "../models/user.js";
 
 
 /**
  * 
  * @param {Number} page 
- * @returns
+ * @returns { Promise<User[]>}
  */
 export const loadUsersByPage = async( page = 1 ) =>{   //recibo el page y sino recibo nada le doy el valor 1 por defecto.
     // esta linea de abajo es lo mismo que http://localhost:3001/users?_page=2
     const url = `${ import.meta.env.VITE_BASE_URL }/users?_page=${ page }`;          //lo defino en mis varialbes de entorno archivo ".env"
     const res = await fetch(url);
-    const data = await res.json();
+    const resjson = await res.json();
+    const data = resjson.data;
 
-    console.log(data);
+//  const users = data.map( userLike => localhostUserToModel( userLike ) ); optimizo
+    const users = data.map( localhostUserToModel );
+    
+//  console.log(users); compruebo el retorno de mi funcion
+    return users;
 }
